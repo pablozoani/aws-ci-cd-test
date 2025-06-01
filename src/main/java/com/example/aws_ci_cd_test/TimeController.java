519449c2d44873f09ec3c027d2334997cce7eaac
Pablo Zoani <pablozoani@hotmail.com>
2025-06-01 06:38:34 -0300

    Initial commit

---
+++ src/main/java/com/example/aws_ci_cd_test/TimeController.java
@@ -0,0 +1,17 @@
+package com.example.aws_ci_cd_test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+public class TimeController {
+
+    @GetMapping("/time")
+    public ResponseEntity<?> getTime() {
+        return ResponseEntity.ok(LocalTime.now());
+    }
+}
